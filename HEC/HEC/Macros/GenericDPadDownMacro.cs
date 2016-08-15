d7af689a66455c3e7a9df42599f4ea8a1d27de4d
Jered Hoff <jeredhoff3@gmail.com>
2016-08-14 20:14:56 -0500

    Generic & VLC

---
+++ HEC/HEC/Macros/GenericDPadDownMacro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HEC.Commands;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace HEC.Macros
+{
+    public class GenericDPadDownMacro : Macro
+    {
+        public GenericDPadDownMacro()
+        {
+            this.Title = "DPad Down";
+            this.AddCommand(new KeyboardCommand(VirtualKeyCode.DOWN, KeyStates.PRESS));
+        }
+    }
+}
